9e6d6e49c712069175305ed16f45f843ce29d87e
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2019-03-20 16:23:49 +0100

    Add files via upload

---
+++ JS/js.js
@@ -0,0 +1,121 @@
+//------------------------------------------------
+// ejercicio de preguntar al usuario cuatro mierdas y luego juntarlas en una frase, pero OJO, solo en la consola.
+//
+//var nombre=prompt("Que pasa, como te llamas? ")
+//var apellido=prompt ("y apellidos por favor ")
+//var edad= prompt (" y ya para completarlo todo, que edad tienes? ")
+
+//esto es lo que deja el mensaje en la consola "console.log"
+// console.log ("Este usuario se llama " + nombre + ", dice que su apellido es " + apellido )
+// console.log("y finalmente, su edad es de " + edad )
+
+
+// -----------------------------------------------
+//
+// Esto es un ejemplo simpre para hacer una calculadora ( multiplicas la edad por 365.25)
+// es decir, solo multiplicas la edad por 365,25, no los dias que has vivido, solo cambiará si cambia la edad 
+
+var edad = prompt ("Dime que edad tienes y te dire cuantos dias has vivido.")
+var year = edad * 365.25
+alert(edad + " years are, more or less, " + year + " days")
+console.log ("el tio ha vivido " + year + " dias" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// cosas que no son del curso.
+
+// -----------------------------------------------
+
+// esto es una funcion que se llama a si misma, se llama recursion.
+// Recursion is a process ( a function, en este caso ) that calls itself
+//
+//
+// function countdown(num){
+// 	if (num <=0) {
+// 		console.log("De puta madre!, no tengo ni idea que he hecho!")
+// 		return;
+// 	}
+// 	console.log(num);
+// 	num--;
+// 	countdown(num);
+// }
+// 
+// countdown(4);
+// 	
+// 	countdown(3);
+// 		
+// 		countdown(2);
+// 			
+// 			countdown(1);
+// 				
+// 				 countdown(0);
+ 				 	
+
+
+//-----------------------------------------------
+
+// function sumRange(num){ 
+// 		if(num === 1) return 1;
+// 		return num + sumRange(num-1);					
+// }
+// 
+// sumRange(3)
+// 
+
+
+
+//-----------------------------------------------
