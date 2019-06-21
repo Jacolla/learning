0c1257a96057941725107cc02a9ec7751cf45afb
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2019-06-21 22:53:10 +0200

    Update phyton.py

--- phyton/phyton.py
+++ phyton/phyton.py
@@ -4,6 +4,8 @@
 
 # msg = ("toma yaah")
 # print(msg)
+
+
 # _______________________________________________
 #
 # numero1=10
@@ -18,6 +20,7 @@
 #     print("Pues no tanto")
 # 
 
+
 # _______________________________________________
 # 
 # def mensajito():                    # Defines 
@@ -31,6 +34,7 @@
 # mensajito()                         # ||
 # mensajito()                         # |
 
+
 # _______________________________________________
  
 # def suma(num1, num2):
@@ -43,9 +47,10 @@
 # 
 # print(almacena_resultado)
 
+
 # _______________________________________________
 
-# Lista0=["Marta", True, 25, "False?"]
+# Lista0=["Marta", True, 30, "False?"]
 # Lista0.append("Sandra")
 # Lista0.insert(1,"Jesusito")
 # Lista0.extend(["Antonio","Alia", "Pepote"])
@@ -55,3 +60,18 @@
 # Lista2=["Charlene", "Janet"]
 # Lista3= Lista0 + Lista1 + Lista2
 # print(Lista3*2)
+
+
+# _______________________________________________
+
+ #  -- Tuplas --
+
+#  Es como una lista, pero no es modificable, es decir, se pueden coger fracciones de la tupla, pero
+#  no puedes quitar/añadir componentes de la tupla, util cuando se quiere mirar la tupla o comprobar si hay algo en la tupla.
+#  Ocupan menos espacio, son mas rapidas de ejecutar, formatean strings
+#  
+#  Diferencia entre tupla y lista a efectos reales?, las listas utilizan los [] y en las tuplas se utilizan ()(que no son obligatorios)
+
+tuplaPrueba=("Marta", 30, "Alia", 25, "Janet", 23 )
+listaDos=list(tuplaPrueba)
+print(tuplaPrueba)
