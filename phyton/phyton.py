0b699b4088b83953e5b1ea64949949aa76abeeae
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2019-07-21 01:35:46 +0200

    Update phyton.py

--- phyton/phyton.py
+++ phyton/phyton.py
@@ -230,18 +230,19 @@
 #     print("No has entendido nada...")
 
 
-# print("Que moto vas a querer?")
-# motos={1:"CB650R",2:"STREET TRIPLE",3:"GSX750",4:"AFRICA TWIN",}
-# #eleccion=motos.lower()
-# 
-# print("1- Honda CBR 650 R")
-# print("2- Triumph street triple")
-# print("3- Suzuki GSX750")
-# print("4- Honda Africa Twin, CRF1000L")
-# 
-# select=int(input("Numero de la afortunada? "))
-# 
-# if select in (1,2,3,4):
-#  print("La escogida es " + motos[select])
-# else:
-#     print("Tio... una de estas...")
+print("Que moto vas a querer?")
+motos={1:"CB650R",2:"STREET TRIPLE",3:"GSX750",4:"AFRICA TWIN",}
+#eleccion=motos.lower()
+
+print("1- Honda CBR 650 R")
+print("2- Triumph street triple")
+print("3- Suzuki GSX750")
+print("4- Honda Africa Twin, CRF1000L")
+
+select=int(input("Numero de la afortunada? "))
+
+if select in (1,2,3,4):
+ print("La escogida es " + motos[select])
+else:
+    print("Tio... una de estas...")
+#punto verde
