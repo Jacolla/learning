f8ca5f0b6ad0822bd3109e01db0dbaa9a2f07a50
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2019-11-23 15:30:25 +0100

    LearningjQuery

--- JS/course/jquery/jquery.js
+++ JS/course/jquery/jquery.js
@@ -1,3 +1,27 @@
-alert("Enchufao")
+$    /* Para empezar con jQuery */
 
-// gotta get back, tomorrow time to jQuery
+////////////////////////////////////
+
+$("ul")     /* Para coger lo que se deseé */
+
+////////////////////////////////////
+
+$("ul").css("color","green")       /* Para modificarle el CSS, ("property","value") */
+
+////////////////////////////////////
+
+let style={
+    color: "purple",
+    background: "green",
+    border: "2px solid blue", 
+}
+
+$("h1").css(style)              /* Se le aplica el style guardado, sin comillas */
+
+////////////////////////////////////
+
+$("li").css({                       /* Tambien se puede crear objeto */
+    fontSize: "30px",               /* Se utiliza cameCase */
+    border: "3px dashed red",
+    background: "blue",             /* RGBA tambien, pero no se por que cojones no me funcionaba */
+})
