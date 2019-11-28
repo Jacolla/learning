3b84a8160f679557d4101f009958c6a5c1c5a0e1
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2019-11-28 02:49:55 +0100

    palanteOtraVez

--- JS/course/jquery/jquery.js
+++ JS/course/jquery/jquery.js
@@ -1,27 +1,64 @@
-$    /* Para empezar con jQuery */
+// $    /* Para empezar con jQuery */
+// 
+// ////////////////////////////////////
+// 
+// $("ul")     /* Para coger lo que se desée */
+// 
+// ////////////////////////////////////
+// 
+// $("ul").css("color","green")                     /* Para modificarle el CSS, ("property","value") */
+// 
+// ////////////////////////////////////
+// 
+// let style={
+//     color: "purple",
+//     background: "green",
+//     border: "2px solid blue", 
+// }
+// 
+// $("h1").css(style)                               /* Se le aplica el style guardado, sin comillas */
+// 
+// ////////////////////////////////////
+// 
+// $("li").css({                                    /* Tambien se puede crear objeto */
+//     fontSize: "30px",                            /* Se utiliza camelCase */
+//     border: "3px dashed red",
+//     background: "blue",             
+// })
+// 
+// ////////////////////////////////////
+// 
+//  $("div").first().css({                          /* con el .first() , se selecciona el primero  */
+//  	color: "pink",
+//  })
+//  
+//  $("#third").css({
+//  	border: "2px solid orange",
+//  })
+//  
+//  $(".higlight").css({
+//  	width: "200px",
+//  })
+//  
+//  $("div").css({
+//  	background: "purple",
+//  })
+//
+// ////////////////////////////////////
 
-////////////////////////////////////
 
-$("ul")     /* Para coger lo que se deseé */
+$("li").text("palante")                             /* con el .text , seleccionas el texto, ("con esto le das su nuevo valor") */
 
-////////////////////////////////////
 
-$("ul").css("color","green")       /* Para modificarle el CSS, ("property","value") */
+$("div").last().fadeOut("slow")                     /* con el faceOut() , va desapareciento (el tiempo estandar es; 400 ) */
 
-////////////////////////////////////
+                                                    /* Habrá que leerse el api.jquery.com */
 
-let style={
-    color: "purple",
-    background: "green",
-    border: "2px solid blue", 
-}
 
-$("h1").css(style)                  /* Se le aplica el style guardado, sin comillas */
 
-////////////////////////////////////
 
-$("li").css({                       /* Tambien se puede crear objeto */
-    fontSize: "30px",               /* Se utiliza camelCase */
-    border: "3px dashed red",
-    background: "blue",             
-})
+
+
+
+
+
