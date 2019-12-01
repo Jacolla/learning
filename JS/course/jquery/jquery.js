954e44c702ae05e7b0a51c948783850650f66823
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2019-12-01 15:49:51 +0100

    newStaff

--- JS/course/jquery/jquery.js
+++ JS/course/jquery/jquery.js
@@ -53,3 +53,24 @@
 //  $("li").html("<a href='http://cabotjaime.com/'>Aprietame</a>")      /* Para modificar el html, poner despues del selector .html ,  y ya con eso tira palante */   
                                                         
                                                         /* Habrá que leerse el api.jquery.com */
+
+// ////////////////////////////////////
+
+                                                        /* el "attr" modifica las propiedades del HTML, pudiendo cambiar el propio html, tambien. */
+//  $("img").last().attr("src", "https://images.unsplash.com/photo-1575096644884-85c5fc58139a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=706&q=80")
+ 
+
+$("img").attr({
+    width: "300px",
+    height:"400px",
+})
+
+$("img").css({
+    margin: "2% auto",
+    border: "2px solid steelblue",
+})
+
+// ////////////////////////////////////
+
+
+$("input").val("tirapayá")                 /* Con el "val()" ,  se le puede mostrar/modificar el valor del atributo*/
