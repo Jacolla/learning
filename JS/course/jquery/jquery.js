3305960690adfa7e30beef252bccc5163493f340
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2019-12-06 16:01:45 +0100

    greenDotAndSomeCode

--- JS/course/jquery/jquery.js
+++ JS/course/jquery/jquery.js
@@ -6,8 +6,8 @@
 // 
 // ////////////////////////////////////
 // 
-// $("ul").css("color","green")                     /* Para modificarle el CSS, ("property","value") */
-// 
+// $("ul").css("color","green")                                         /* Para modificarle el CSS, ("property","value") */
+//                                                                      /* Un argumento podria ser un "className" por ejemplo */
 // ////////////////////////////////////
 // 
 // let style={
@@ -16,27 +16,27 @@
 //     border: "2px solid blue", 
 // }
 // 
-// $("h1").css(style)                               /* Se le aplica el style guardado, sin comillas */
+// $("h1").css(style)                                                   /* Se le aplica el style guardado, sin comillas */
 // 
 // ////////////////////////////////////
 // 
-// $("li").css({                                    /* Tambien se puede crear objeto */
-//     fontSize: "30px",                            /* Se utiliza camelCase */
+// $("li").css({                                                        /* Tambien se puede crear objeto */
+//     fontSize: "30px",                                                /* Se utiliza camelCase */
 //     border: "3px dashed red",
 //     background: "blue",             
 // })
 // 
 // ////////////////////////////////////
 // 
-//  $("div").first().css({                          /* con el .first() , se selecciona el primero  */
+//  $("div").first().css({                                              /* con el .first() , se selecciona el primero  */
 //  	color: "pink",
 //  })
 //  
-//  $("#third").css({                               /* "#third " para seleccionar "ID" */
+//  $("#third").css({                                                   /* "#third " para seleccionar "ID" */
 //  	border: "2px solid orange",
 //  })
 //  
-//  $(".highlight").css({                            /* ".highlight" para seleccionar "class" */
+//  $(".highlight").css({                                               /* ".highlight" para seleccionar "class" */
 //  	width: "200px",
 //  })
 //  
@@ -46,32 +46,37 @@
 //
 // ////////////////////////////////////
 //  
-//  $("li").text("palante")                             /* con el .text , seleccionas el texto, ("con esto le das su nuevo valor") */
+//  $("li").text("palante")                                             /* con el .text , seleccionas el texto, ("con esto le das su nuevo valor") */
 //  
-//  $("div").last().fadeOut("slow")                     /* con el faceOut() , va desapareciento (el tiempo estandar es; 400 ) */
+//  $("div").last().fadeOut("slow")                                     /* con el faceOut() , va desapareciento (el tiempo estandar es; 400 ) */
 //  
 //  $("li").html("<a href='http://cabotjaime.com/'>Aprietame</a>")      /* Para modificar el html, poner despues del selector .html ,  y ya con eso tira palante */   
-                                                        
-                                                        /* Habrá que leerse el api.jquery.com */
-
-// ////////////////////////////////////
-
-                                                        /* el "attr" modifica las propiedades del HTML, pudiendo cambiar el propio html, tambien. */
+                                                                        /* Habrá que leerse el api.jquery.com */
+                                                                        
+                                                                        /* el "attr" modifica las propiedades del HTML, pudiendo cambiar el propio html, tambien. */
 //  $("img").last().attr("src", "https://images.unsplash.com/photo-1575096644884-85c5fc58139a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=706&q=80")
  
+//  
+//  $("img").attr({
+//      width: "300px",
+//      height:"400px",
+//  })
+//  
+//  $("img").css({
+//      margin: "2% auto",
+//      border: "2px solid steelblue",
+//  })
 
-$("img").attr({
-    width: "300px",
-    height:"400px",
-})
-
-$("img").css({
-    margin: "2% auto",
-    border: "2px solid steelblue",
-})
+//$("input").val("tirapayá")                                            /* Con el "val()" ,  se le puede mostrar/modificar el valor del atributo*/
 
 // ////////////////////////////////////
-
-
-$("input").val("tirapayá")                 /* Con el "val()" ,  se le puede mostrar/modificar el valor del atributo*/
-
+//  
+//  Otras herramientas útiles para manipular con jQuery
+//  
+//  val()                           
+//  attr()                          
+//  text()                          
+//  html()                          
+//  addClass()                      
+//  removeClass()                   
+//  toggle()                        
