09763e699e9d9dbb0fb8d38bb60cc4e2a1b2d694
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2019-12-25 23:20:29 +0100

    palante

--- JavaScript/jquery/jquery.js
+++ JavaScript/jquery/jquery.js
@@ -13,8 +13,8 @@
 // 
 // ////////////////////////////////////
 // 
-// $("ul").css("color","green")                                         /* Para modificarle el CSS, ("property","value") */
-//                                                                      /* Un argumento podria ser un "className" por ejemplo */
+// $("ul").css("color","green")                                             /* Para modificarle el CSS, ("property","value") */
+//                                                                          /* Un argumento podria ser un "className" por ejemplo */
 // ////////////////////////////////////
 // 
 // let style={
@@ -23,27 +23,27 @@
 //     border: "2px solid blue", 
 // }
 // 
-// $("h1").css(style)                                                   /* Se le aplica el style guardado, sin comillas */
+// $("h1").css(style)                                                       /* Se le aplica el style guardado, sin comillas */
 // 
 // ////////////////////////////////////
 // 
-// $("li").css({                                                        /* Tambien se puede crear objeto */
-//     fontSize: "30px",                                                /* Se utiliza camelCase */
+// $("li").css({                                                            /* Tambien se puede crear objeto */
+//     fontSize: "30px",                                                    /* Se utiliza camelCase */
 //     border: "3px dashed red",
 //     background: "blue",             
 // })
 // 
 // ////////////////////////////////////
 // 
-//  $("div").first().css({                                              /* con el .first() , se selecciona el primero  */
+//  $("div").first().css({                                                  /* con el .first() , se selecciona el primero  */
 //  	color: "pink",
 //  })
 //  
-//  $("#third").css({                                                   /* "#third " para seleccionar "ID" */
+//  $("#third").css({                                                       /* "#third " para seleccionar "ID" */
 //  	border: "2px solid orange",
 //  })
 //  
-//  $(".highlight").css({                                               /* ".highlight" para seleccionar "class" */
+//  $(".highlight").css({                                                   /* ".highlight" para seleccionar "class" */
 //  	width: "200px",
 //  })
 //  
@@ -53,25 +53,25 @@
 //
 // ////////////////////////////////////
 //  
-//  $("li").text("palante")                                             /* con el .text , seleccionas el texto, ("con esto le das su nuevo valor") */
+//  $("li").text("palante")                                                 /* con el .text , seleccionas el texto, ("con esto le das su nuevo valor") */
 //  
-//  $("div").last().fadeOut("slow")                                     /* con el faceOut() , va desapareciento (el tiempo estandar es; 400 ) */
+//  $("div").last().fadeOut("slow")                                         /* con el faceOut() , va desapareciento (el tiempo estandar es; 400 ) */
 //  
-//  $("li").html("<a href='http://cabotjaime.com/'>Aprietame</a>")      /* Para modificar el html, poner despues del selector .html ,  y ya con eso tira palante */   
+//  $("li").html("<a href='http://cabotjaime.com/'>Aprietame</a>")          /* Para modificar el html, poner despues del selector .html ,  y ya con eso tira palante */   
                                                                         
-                                                                        /* el "attr" modifica las propiedades del HTML, pudiendo cambiar el propio html, tambien. */
+                                                                            /* el "attr" modifica las propiedades del HTML, pudiendo cambiar el propio html, tambien. */
 //  $("img").last().attr("src", "https://images.unsplash.com/photo-1575096644884-85c5fc58139a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=706&q=80")
  
 // ////////////////////////////////////
 
-$("ul").css({
-    width: "50px",
-    height:"60px",
-    margin: "2% ",
-    border: "2px solid steelblue",
-})
+//  $("ul").css({
+//      width: "50px",
+//      height:"60px",
+//      margin: "2% ",
+//      border: "2px solid steelblue",
+//  })
 
-//$("input").val("tirapayá")                                            /* Con el "val()" ,  se le puede mostrar/modificar el valor del atributo*/
+//$("input").val("tirapayá")                                                /* Con el "val()" ,  se le puede mostrar/modificar el valor del atributo*/
 
 // ////////////////////////////////////
 //  
@@ -83,17 +83,66 @@ $("ul").css({
 //  html()                          
 //  addClass()                      
 //  removeClass()                   
-//  toggleClass()                                                       /* Especie de interruptor */   
+//  toggleClass()                                                           /* Especie de interruptor */   
 
 // ////////////////////////////////////
-// truquillo del greenDot
-// /* Para hacer funciones en jQuery */
-// ej:
-$("button").click(function(){
-        /* y pues a darle */
-        $(this).css("background","pink");
-        let text= $(this).text();
-        console.log("Ha apretado: " + text)
-        console.log("como te gusta apretar...")
-});
+
+//  truquillo del greenDot
+//  /* Para hacer funciones en jQuery */
+//  ej:
+//  $("button").click(function(){
+//          /* y pues a darle */
+//          $(this).css({
+//              color: "red",
+//              background: "pink",
+//          });
+//  
+//          alert("CUIDAO!")
+//          let text= $(this).text();
+//          console.log("Ha apretado: " + text)
+//  });
     
+// ////////////////////////////////////
+
+//  $("#texto").keypress(function(event){                                   /* Con el objeto event, capturamos la input en la variable */
+//  	if(event.which === 13){                                             /* Para cuando se aprieta la tecla 13 ( enter en este caso ) */
+//  		alert("apretaste ENTER!")                                       /* Funcion que salta cuando se ha apretado la tecla 13 */
+//  	}
+//  });
+
+$("li").on("click", function(){                                             /* El .on , funciona tipo "add", no ejecuta la funcion hasta */
+	$(this).css({                                                           /* que se lo clickea, en este caso  */
+		color: "green",                                                     /* con el this , seleccionamos el clickeado  */
+		background: "blue",
+		height: "50px",
+		width: "150px",
+});
+})
+
+$("button").on("mouseenter", function(){                                    /* mouseenter es para cuando el raton pasa por encima del button, en este caso */
+    $(this).css("font-weight","bold");
+});
+
+$("button").on("mouseleave", function(){                                    /* mouseleave, para cuando ya no está encima del button, en este caso */
+    $(this).css("font-weight","normal");                                    /* rollo hover de css */
+});
+
+
+$("button").on("click", function(){ 
+	$(this).css({               
+    });
+    alert("Joder, que fuerte has apretado, pues ahora me voy.")
+})
+
+
+// Why use on()?
+// More or less both will have the job document, but there is a difference
+// click(), only adds listeners for existing elements 
+// on(), will add listener for all potential future elements.
+
+// ////////////////////////////////////
+
+$("button").click(function(){
+    $("html").fadeOut("slow", function () {
+    })
+})
