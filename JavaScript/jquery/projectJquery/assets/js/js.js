4dafe9a907faa316712fc7fb96eccf46fbc3d114
Jacolla <colladocabot@gmail.com>
2020-01-04 15:49:23 +0100

    palante

--- JavaScript/jquery/projectJquery/assets/js/js.js
+++ JavaScript/jquery/projectJquery/assets/js/js.js
@@ -1,40 +1,41 @@
-
-        /* Los "li's" */
-$("li").mouseenter(function(){
-    $(this).css({
-        color: "pink",    
-        background: "blue",
-    });
-})
-
-$("li").mouseleave(function(){
-    $(this).css({
-        color: "black",    
-        background: "steelblue",
-    });
-})
-    /*------------------------*/
-
-
         /* Los "buttons" */
 $("button").mouseenter(function(){
     $(this).css({
-        fontSize: "2em",    
+        fontSize: "26px",    
     });
 
 })
 
 $("button").mouseleave(function(){
     $(this).css({
-        fontSize: "13px",               /* Tamaño por defecto, en mi PC */
+        fontSize: "13.4px",               /* Tamaño por defecto, en mi PC */
     });
 })
 
-
-
-$(this).click(function(){
-    $("li").css({
-        background: "purple",
-    });
-})
     /*------------------------*/
+
+
+/* Método clasico viejo */
+//  $("li").click(function(){
+//      if ($(this).css("color") === "rgb(128, 128, 128)"){
+//          $(this).css({
+//              color: "black",
+//              textDecoration: "none",
+//              backgroundColor: "aqua",
+//  
+//  
+//          })
+//      }
+//      else{
+//      $(this).css({
+//          color: "gray",
+//          backgroundColor: "pink",
+//          textDecoration: "line-through",
+//          });
+//      }
+//  });
+
+/* jQuery en 2 lineas */
+$("li").click(function(){
+    $(this).toggleClass("completado");          /* Aquí es donde se pone el Toggle */
+ });
