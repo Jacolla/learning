9d0750cac638638e639c9e63c164a5fe03afc9a0
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2020-01-05 02:45:43 +0100

    memara

--- JavaScript/jquery/projectJquery/assets/js/js.js
+++ JavaScript/jquery/projectJquery/assets/js/js.js
@@ -1,13 +1,41 @@
+        /* Los "buttons" */
+$("button").mouseenter(function(){
+    $(this).css({
+        fontSize: "26px",    
+    });
 
+})
+
+$("button").mouseleave(function(){
+    $(this).css({
+        fontSize: "13.4px",               /* Tamaño por defecto, en mi PC */
+    });
+})
+
+    /*------------------------*/
+
+
+/* Método clasico viejo */
+//  $("li").click(function(){
+//      if ($(this).css("color") === "rgb(128, 128, 128)"){
+//          $(this).css({
+//              color: "black",
+//              textDecoration: "none",
+//              backgroundColor: "aqua",
+//  
+//  
+//          })
+//      }
+//      else{
+//      $(this).css({
+//          color: "gray",
+//          backgroundColor: "pink",
+//          textDecoration: "line-through",
+//          });
+//      }
+//  });
+
+/* jQuery en 2 lineas */
 $("li").click(function(){
     $(this).toggleClass("completado");          /* Aquí es donde se pone el Toggle */
  });
-
-
-
- 
- /*-----------------------------------------*/
- 
-$('html, body').animate({
-    scrollTop: $("#target").offset().top
-}, 1000);
