7a64245a3a31bcaa0f7efb8c636b1fbe9a6c2872
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2020-01-20 15:01:58 +0100

    posEnga

--- SQL/testingSQL.sql
+++ SQL/testingSQL.sql
@@ -1,8 +1,18 @@
-CREATE TABLE baseDatos (
-   probandoSQL TEXT,
-   probandoSQL2 INTEGER,
-   probandoSQL3 TEXT NOT NULL,
-   probandoSQL4 DEFAULT 'not Applicable',
+/* Vamos a ver funciona rollo cuadrado(TABLE), que es lo que almacena la información(DATA),
+   que lo va acumulando en columnas(COLUMNS) y listas(ROW).
 
-/* puesAqui */
- );
+   Para crearla, se la invoca con CREATE, pám, ahora le dices qué crear,
+   pues le decímos que una tablíta(TABLE), despues le damos nombre 
+   y parentesis para meterle la chicha
+
+ */
+
+ CREATE TABLE tiendaClassic(
+   product INTEGER(50),     /* detrás va el tipo de data */
+   workers INTEGER(4),      /* camelCase para el tipo de data */    
+   clientes INTEGER(100),
+   
+);   /* Detrás va el semicolon */
+
+ INSERT INTO tiendaClassic(product, workers, clientes)
+ VALUES (2);
