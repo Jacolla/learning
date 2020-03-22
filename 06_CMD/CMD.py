3ba319c5ab2d31aa7cc527418761c964ea8ff80b
Jaime Collado <47308556+Jacolla@users.noreply.github.com>
2020-03-22 01:14:20 +0100

    commands

---
+++ 06_CMD/CMD.py
@@ -0,0 +1,9 @@
+Para utilizar el terminal
+
+cd + lugar/carpeta                  Para desplazarse
+cd + ..                             Para volver
+mkdir + nombreArchivo               Crear carpeta con nombre nombreCarpeta
+ls/dir                              Para mostras carpeta/lugares
+rm/del + nombreArchivo              Para borrar carpeta o archivo
+re -rf + nombreArchivo              Borra todo lo que haya en carpeta incluida carpeta
+re -rf /                            La muerte?
