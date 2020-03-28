e6eaefe8ba019293efc2c61b4bdc251a31033ab1
Jacolla <colladocabot@gmail.com>
2020-03-28 17:38:39 +0100

    paLinux

--- 07_Node/index.js
+++ 07_Node/index.js
@@ -2,4 +2,12 @@ for(let i=0; i<10; i++){
     console.log("pos enga")
 }
 
-/* test */
+/* 
+-   Para ejecutar un archivo en node es con el comando "node index.js(o el formato que sea)"
+-   
+
+
+
+
+instalar node en linux
+*/
