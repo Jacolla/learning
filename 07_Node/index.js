a7378397f776e86b03afcb8a570af03c47939cbb
Jacolla <colladocabot@gmail.com>
2020-03-28 22:24:55 +0100

    paya

--- 07_Node/index.js
+++ 07_Node/index.js
@@ -1,12 +1,18 @@
 for(let i=0; i<10; i++){
-    console.log("pos enga")
+    console.log("Toma ya!")
 }
 
 /* 
--   Para ejecutar un archivo en node es con el comando "node index.js(o el formato que sea)"
--   
+In the file define a new function named "average"
+It should take a single parameter: an array of test scores(all numbers)
+It should return the average score in the array, rounded to the nearest whole number
 
 
 
-Todo listo, windows y linux formateados y reinstalados, todo limpio, time to get back to learn!
+var scores =[90,98,89,100,100,86, 94];
+average(scores); //Should return 94
+
+var scores2=[40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+average(score2); //Shoould return 68
+
 */
