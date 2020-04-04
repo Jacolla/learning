194e9f607a87d83ee5234a9737d287dae3cd4a07
Jacolla <colladocabot@gmail.com>
2020-04-04 15:43:45 +0200

    gettingLibs

--- 06_CMD/CMD.py
+++ 06_CMD/CMD.py
@@ -3,6 +3,7 @@
 # cd + lugar/carpeta                  Para desplazarse
 # cd + ..                             Para volver
 # mkdir + nombreArchivo               Crear carpeta con nombre nombreCarpeta
+# touch + perroCagando.js             Para crear archivo js en este caso
 # ls/dir                              Para mostras carpeta/lugares
 # rm/del + nombreArchivo              Para borrar carpeta o archivo
 # re -rf + nombreArchivo              Borra todo lo que haya en carpeta incluida carpeta
---
+++ 07_Node/MyShop/index.js
@@ -0,0 +1,11 @@
+let faker = require("faker");
+
+let randomNombre = faker.name.firstName();
+let numero = faker.random.number();
+let raroNombre = faker.name.firstName();
+
+
+
+console.log(randomNombre);
+console.log(nombreRaro);
+console.log(raroNombre);
