c547345d49341310992706a90a91771592df3d08
Jacolla <colladocabot@gmail.com>
2020-04-19 16:00:25 +0200

    exercise_with_express

--- 08_Express/app.js
+++ 08_Express/app.js
@@ -7,7 +7,7 @@ app.listen(3000, function () {
 });
 
 app.get('/', function (req, res) {
-    res.send('ahora otra cosa!');
+    res.send('Bienvenido al puerto 3000 compare!');
 });
 
 app.get("/adios", function(req, res){
@@ -18,9 +18,15 @@ app.get("/entrada", function(req, res){
     res.send("Hola, bienvenido a mi servidor, estas hospedado en el puerto3000")
 })
 
+
+app.get('/r/:section/comments/:id/:tittle', function (req, res){
+    var identification= req.params.id;
+    var sect= req.params.section;
+    res.send("Bienvenido a " + sect + " de " + identification + ", supare.");
+    console.log(req.params)
+});
+
 app.get("*", function(req, res){
     res.send("Caballero, eso no lo tenemos en nuestra base de datos.");
 })
 
-
-// todayIsTimeToRead codeComplete vol.2
--- 08_Express/notes.md
+++ 08_Express/notes.md
@@ -13,6 +13,17 @@ Nodemon, hace que en vez de tener  que reiniciar el servidor,
 se "actualiza" sin tener que reiniciar el servidor entero,
 con refrescar la página basta.
 
-Para ejecutarlo igual que node
-nodemon "app.js"
+Para ejecutarlo igual que node; nodemon "app.js"
+
+con los ":"
+se le dice que todo lo que vayas detras de los :
+está incluido en la seccion ya indicada
+
+
+Para ir cogiendo info dáda al servidor, hay que envolverla en una 
+variable, una vez envuelta ya podemos jugar con ella como otro objeto, ej;
+var/let identificacion = req.parms."nombre_de_parametro"
+
+
+
 -->
