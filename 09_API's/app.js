cbd9d6b892b95994e191b1ec88410b8a810a15cf
Jacolla <colladocabot@gmail.com>
2020-05-22 22:37:14 +0200

    posEnga

---
+++ 09_API's/app.js
@@ -0,0 +1,11 @@
+let request = require("request");
+
+request("https://jsonplaceholder.typicode.com/users/1", function(error, response, body){
+    if(!error && Response.statusCode == 200){
+        eval(require("locus"));
+        let parseData = JSON.parse(body);
+        console.log(parseData["query"],["results"],["astronomy"],["sunset"]);
+    };
+});
+
+// Pues na, esto para el gitHub
