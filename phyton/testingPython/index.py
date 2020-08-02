32415dff67fad85a3150a0bfe354ea3529dabf47
Jacolla <colladocabot@gmail.com>
2020-08-02 21:48:55 +0200

    chill

--- phyton/testingPython/index.py
+++ phyton/testingPython/index.py
@@ -1,14 +1,21 @@
 sayMyName = "myname"
-resQuest = (input("Nombre: "))     
+resQuest = (input("Name: "))     
+
 while resQuest != sayMyName:
         print("No lad no... say my name ")
         desbucleando = input("¿?")
+        
         if desbucleando == sayMyName:
             print("Aight, it's done, you free to go.") 
             valorat = input("Have you enjoyed? ")
-            while valorat != "hell yes":
+            
+            while valorat != "yes":
                 print("¿What could you improve?")
                 input("¿Mix of all?")
+
         elif desbucleando == str():
             print("Ahora me estas empezando a gustar,")        
-            break #(dance, isn't it?, nah... maybe not...)
+            
+
+
+# but now I'm chilling a bit...
