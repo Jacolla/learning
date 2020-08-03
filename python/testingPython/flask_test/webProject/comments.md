0897b8678d58af64e218d06da48e4fe4723aadc7
Jacolla <colladocabot@gmail.com>
2020-08-04 00:10:54 +0200

    flask

---
+++ python/testingPython/flask_test/webProject/comments.md
@@ -0,0 +1,5 @@
+Vale amo a ver, el rollo base es el mismo en todos lados (creo),
+pero en python es bastante mas facil y se entiende mucho mejor.
+
+El venv, creo que es rollo el "node", pero otro estilo.
+el Flask creo que es tipo libreria que hace las bases para sacarlo a web.
---
+++ python/testingPython/flask_test/webProject/index.py
@@ -0,0 +1,26 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to my Web page in Python'
+
+@app.route('/home')
+def home():
+    return "This is home"
+
+@app.route('/profile')
+def profile():
+    return "This is profile"
+    
+@app.route('/personal')
+def personal():
+    return "Here we do have our personal space "
+    
+@app.route('/posts')
+def posts():
+    return "And over here is the posts area"
+    
+@app.route('/comments')
+def comments():
+    return "Here goes comments of people."
