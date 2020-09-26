8de8fad80e4c17e9eeb30098aeb38e9ad1c38bbd
jacolla <colladocabot@gmail.com>
2020-09-26 11:20:42 +0200

    RIP_i52500K

--- python/testingPython/flask_test/webProject/index.py
+++ python/testingPython/flask_test/webProject/index.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 def entrada():
     return render_template('index.html')
 if __name__ == '__main__':
-   app.run(debug=True)
+   app.run(debug=False)
+
 
 
 # @app.route('/home')
