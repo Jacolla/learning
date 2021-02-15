2add793bc48bd05f0607a557093c52dbc6cdcb00
Jacolla <colladocabot@gmail.com>
2021-02-15 21:20:51 +0100

    palante

--- python/django+react/musicWeb/api/models.py
+++ python/django+react/musicWeb/api/models.py
@@ -5,8 +5,8 @@ import random
 def generate_unique_code():
     length = 6
     while True:
-        code=''.join(rando.choices(string.ascii_uppercase, k=length))
-        if Room.objects.filter(code=code).count() == 0:
+        code = ''.join(rando.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code = code).count() == 0:
             break
 
         return code
@@ -16,5 +16,5 @@ class Room(models.Model):
     code = models.CharField(max_length=8, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
-    votes_to_skip = models.IntegerField(null=False, default=2)
+    vote_to_skip = models.IntegerField(null=False, default=2)
     created_at = models.DateField(auto_now_add=True)
--- python/django+react/musicWeb/api/serializers.py
+++ python/django+react/musicWeb/api/serializers.py
@@ -4,5 +4,4 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model=Room
-        # fields=('__all__')
-        fields=('id','code','host','guest_can_pause','votes_to_skip','created_at',)
+        fields=('id','code','host','guest_can_pause','vote_to_skip','created_at',)
