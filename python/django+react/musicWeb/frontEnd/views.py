c4751f1eac84403bf17357ca7c2077cbd98bb972
Jacolla <colladocabot@gmail.com>
2021-02-20 20:58:54 +0100

    notDoneYet

--- python/django+react/musicWeb/frontEnd/views.py
+++ python/django+react/musicWeb/frontEnd/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request, *args, **kwargs):
-    return render(request, "front/index.html")
+    return render(request, 'frontEnd/index.html')
 
 
 
