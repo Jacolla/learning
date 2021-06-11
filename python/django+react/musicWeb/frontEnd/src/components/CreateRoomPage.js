a4d83e6cb55fc1dae22058173bb3bb367fe80778
jacolla <colladocabot@gmail.com>
2021-06-11 19:50:04 +0200

    gettingCloser

--- python/django+react/musicWeb/frontEnd/src/components/CreateRoomPage.js
+++ python/django+react/musicWeb/frontEnd/src/components/CreateRoomPage.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
 import { Link } from "react-router-dom";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
+import {Button, Grid, Typography, TextField, FormHelperText,
+        FormControl, FormControlLabel, Radio, RadioGroup, 
+       } from "@material-ui/core"
+
 
 export default class CreateRoomPage extends Component {
   defaultVotes = 5;
@@ -54,27 +49,33 @@ export default class CreateRoomPage extends Component {
   render() {
     return (
       <Grid container spacing={1}>
+
         <Grid item xs={12} align="center">
           <Typography component="h4" variant="h4">
             Create A Room
           </Typography>
         </Grid>
+
         <Grid item xs={12} align="center">
           <FormControl component="fieldset">
+
             <FormHelperText>
               <div align="center">Guest Control of Playback State</div>
             </FormHelperText>
+            
             <RadioGroup
               row
               defaultValue="true"
               onChange={this.handleGuestCanPauseChange}
             >
+
               <FormControlLabel
                 value="true"
                 control={<Radio color="primary" />}
                 label="Play/Pause"
                 labelPlacement="bottom"
               />
+
               <FormControlLabel
                 value="false"
                 control={<Radio color="secondary" />}
@@ -83,8 +84,11 @@ export default class CreateRoomPage extends Component {
               />
             </RadioGroup>
           </FormControl>
+
         </Grid>
+
         <Grid item xs={12} align="center">
+
           <FormControl>
             <TextField
               required={true}
@@ -96,10 +100,13 @@ export default class CreateRoomPage extends Component {
                 style: { textAlign: "center" },
               }}
             />
+
             <FormHelperText>
               <div align="center">Votes Required To Skip Song</div>
             </FormHelperText>
+
           </FormControl>
+
         </Grid>
         <Grid item xs={12} align="center">
           <Button
@@ -110,11 +117,13 @@ export default class CreateRoomPage extends Component {
             Create A Room
           </Button>
         </Grid>
+
         <Grid item xs={12} align="center">
           <Button color="secondary" variant="contained" to="/" component={Link}>
             Back
           </Button>
         </Grid>
+        
       </Grid>
     );
   }
--- python/django+react/musicWeb/frontEnd/src/components/HomePage.js
+++ python/django+react/musicWeb/frontEnd/src/components/HomePage.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,18 +15,77 @@ import {
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: null,                                                               // Para que salga el HomePage crudo, sin room
+    };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
+  }
+
+  async componentDidMount(){                                                        // Con el async le dices que arranque despues de que se carge la pagina. 
+    fetch("/api/user-in-room")
+    .then((response) => response.json())
+    .then((data) => {
+      this.setState({
+        roomCode: data.code,
+      });
+    });
+  }
+
+  renderHomePage(){
+    return(
+      <Grid container spacing={3}>
+        
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            Music Rooms
+          </Typography>
+        </Grid>
+        
+        <Grid item xs={12} align="center">
+          <ButtonGroup  variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+
+      </Grid>
+    );
+  }
+
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
   }
 
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/">
-            <p>This is the home page</p>
-          </Route>
-            <Route path="/join" component= {RoomJoinPage} />
-            <Route path="/create" component= {CreateRoomPage} />
-            <Route path= "/room/:roomCode" component= {Room} />
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return this.state.roomCode ? (
+                <Redirect to={`/room/${this.state.roomCode}`} />                                             // Para cuando vuelva a misma web, le re-diriga a su misma room, rollo si apaga pc/telf y vuelve a meterse, le salte directamente la misma room
+              ) : (
+                this.renderHomePage()
+              );
+            }}
+          />
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route 
+            path="/room/:roomCode" 
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
         </Switch>
       </Router>
     );
