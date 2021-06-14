71ce9c266bd15e3bd944fff790649c131c4abf27
jacolla <colladocabot@gmail.com>
2021-06-14 15:51:08 +0200

    checkBackEndUpdateRoom

--- python/django+react/musicWeb/api/views.py
+++ python/django+react/musicWeb/api/views.py
@@ -1,7 +1,10 @@
+from django.db.models import query_utils
+from django.db.models.query import QuerySet
+from django.http import response
 from django.http.response import JsonResponse
 from django.shortcuts import render
-from rest_framework import generics, status
-from .serializers import RoomSerializer, CreateRoomSerializer
+from rest_framework import generics, serializers, status
+from .serializers import RoomSerializer, CreateRoomSerializer, UpdateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -102,3 +105,33 @@ class LeaveRoom(APIView):
                 room.delet()
 
         return Response ({"Message" : "Succes"}, status=status.HTTP_200_OK)
+
+
+
+class UpdateView(APIView):
+    serializer_class = UpdateRoomSerializer
+    
+    def patch(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()        
+                                                                                          # normalmente se usa el "patch" para modificar componentes activos.
+        serializer = self.serializaer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get("guest_can_pause")
+            votes_to_skip = serializer.data.get("votes_to_skip")
+            code = serializer.data.get("code")
+            queryset = Room.objects.filter(code=code)
+            if not queryset.exist():
+                return response({"Error" : "Room not found"}, status=status.HTTP_404_NOT_FOUND)
+
+            room = queryset[0]
+            user_id = self.request.session.session_key
+            if room.host != user_id:
+                return Response({"msg" : "You are not the host! Bitch"}, status=status.HTTP_403_FORBIDDEN)
+
+            room.guest_can_pause = guest_can_pause
+            room.votes_to_skyp = votes_to_skip
+            room.save = (update_filds=["guest_can_pause", "votes_to_skip"])
+            return response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+
+        return response({"Bad Request " : "Invalid data..."}, status=status.HTTP_400_BAD_REQUEST) 
